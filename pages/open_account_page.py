eb32db1fca386ed7a5dd708d74ab00eab88a2f31
willzhang-2024 <will19940210@gmail.com>
2025-07-17 14:34:27 +1000

    get dynamic account balance

--- pages/open_account_page.py
+++ pages/open_account_page.py
@@ -1,3 +1,5 @@
+import re
+
 from pages.base_page import BasePage
 from utils import generate_username, dump_to_json, load_json_file_info
 
@@ -20,3 +22,15 @@ class OpenAccountPage(BasePage):
         self.new_account_id = self.right_panel.locator('#newAccountId').text_content()
         dump_to_json('account_info.json', 'new account', self.new_account_id)
         return self.new_account_id
+
+    def get_new_account_balance(self):
+        balance_text = self.right_panel.locator('p:below(#type)').text_content()
+        # Regular expression to match a monetary value
+        match = re.search(r"\$([0-9]+(?:\.[0-9]{2})?)", balance_text)
+
+        if match:
+            amount = match.group(1)  # Convert the matched value to a float
+            print(amount)  # Output: 100.0
+            return amount
+        else:
+            print("No monetary value found.")
